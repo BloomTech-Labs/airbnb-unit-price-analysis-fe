f43457caa8fe325da77db48973c437a9c508d4bd
springercass <casprin2@gmail.com>
2019-09-25 19:04:03 -0500

    landing page styling WIP

--- airbnb-fe/src/components/confirmation/Text.js
+++ airbnb-fe/src/components/confirmation/Text.js
@@ -1,49 +1,65 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    width: 70%;
-    height: 100%;
-`
+  width: 70%;
+  height: 100%;
+`;
 
 const H1 = styled.h1`
-    margin: 0px;
-    margin-top: 15px;
-`
+  margin: 0px;
+  margin-top: 15px;
+`;
 const Location = styled.div`
-    margin: 0px;
-    font-size: 25px;
-`
+  margin: 0px;
+  font-size: 25px;
+`;
 
 const Feature = styled.div`
-    margin: 18px 0px 0px;
-    font-size: 20px;
-    font-weight: 600;
-`
+  margin: 18px 0px 0px;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const PDetails = styled.div`
+  margin: 0px;
+  font-size: 18px;
+  width: 45%;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Detail = styled.div`
-    margin: 0px;
-    font-size: 18px;
-`
+  margin: 0px;
+  font-size: 18px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
 
-function Text(){
-    return(
-        <Container>
+const DetailP = styled.p`
+  margin: 0;
+`;
 
-            <H1>Miami Beach 8 guests 3 Bedroom Apartment (20)</H1>
-            <Location>Miami Beach</Location>
+function Text() {
+  return (
+    <Container>
+      <H1>Miami Beach 8 guests 3 Bedroom Apartment (20)</H1>
+      <Location>Miami Beach</Location>
 
-            <Feature>Entired serviced apartment</Feature>
-            <Detail>8 guests  3 bedrooms  4beds  2baths</Detail>
+      <Feature>Entired serviced apartment</Feature>
+      <PDetails>
+        <DetailP>8 guests</DetailP> <DetailP>3 bedrooms</DetailP>{" "}
+        <DetailP>4beds</DetailP> <DetailP>2baths</DetailP>
+      </PDetails>
 
-            <Feature>Sparkling clean</Feature>
-            <Detail>5 recent guests said this place was sparkling clean</Detail>
+      <Feature>Sparkling clean</Feature>
+      <Detail>5 recent guests said this place was sparkling clean</Detail>
 
-            <Feature>Self check-in</Feature>
-            <Detail>you can check in with the doorman</Detail>      
-                 
-        </Container>
-    );
+      <Feature>Self check-in</Feature>
+      <Detail>you can check in with the doorman</Detail>
+    </Container>
+  );
 }
 
 export default Text;
--- airbnb-fe/src/components/landing/Quadrant1.js
+++ airbnb-fe/src/components/landing/Quadrant1.js
@@ -1,19 +1,18 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
+import Banner from "../../img/LandingBanner.png";
 
-import Header from './Header';
+import Header from "./Header";
 
 const Q1div = styled.div`
-    // height: 45vw;        //<- Dynamic height
-    // min-height: 600px;   //<- Dynamic height
-    height: 77vh;           //<- Static height
-    width: 100%;
-    background-color: grey;
-    display: flex;
-    align-items: center;
-`
-
-
+  // height: 45vw;        //<- Dynamic height
+  // min-height: 600px;   //<- Dynamic height
+  height: 96vh; //<- Static height
+  width: 100%;
+  background-image: url(${Banner});
+  display: flex;
+  align-items: center;
+`;
 
 function Quadrant1() {
   return (
