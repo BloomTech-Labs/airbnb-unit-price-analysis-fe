fa170306a730f0a79537b77bc530b8583becc19d
vladmog <vladmog@gmail.com>
2019-10-16 20:01:21 -0700

    added cors-anywhere to DS endpoint

--- airbnb-fe/src/store/actions/index.js
+++ airbnb-fe/src/store/actions/index.js
@@ -35,7 +35,7 @@ export const getListing = id => dispatch => {
   dispatch({ type: FETCH_LISTING_START });
   axios
     .get(
-      `http://LabsAirbnb-env-dev.us-west-1.elasticbeanstalk.com/data?id=${id}`
+      `https://cors-anywhere.herokuapp.com/http://LabsAirbnb-env-dev.us-west-1.elasticbeanstalk.com/data?id=${id}`
     )
     .then(response => {
       console.log(response);
